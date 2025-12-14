30488f452bde51dbf0e1ab14fdbe469d313091af
RigeQerimi27 <rq73758@ubt-uni.net>
2025-12-14 21:50:20 +0100

    login forms validation
    signup and signin form validation at main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+function validate() {
+
+    let email=document.getElementById("email").value;
+
+    let password=document.getElementById("password").value;
+
+    let emailRegex= /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    let passwordRegex= /^(?=.*\d).{8,}$/;
+
+    if(emailRegex.test(email)){
+
+        if(passwordRegex.test(password)){
+
+            document.getElementById("msg").innerText="Valid!";
+
+            document.getElementById("loginForm").action="homepage.html";
+            document.getElementById("loginForm").submit();
+
+        } else{
+
+            document.getElementById("msg").innerText=
+            "Password must be at least 8 characters and contain a number!";
+        }
+
+
+
+        }else{
+
+            document.getElementById("msg").innerText="Email is not valid!";
+        }
+    }
+
+
+
